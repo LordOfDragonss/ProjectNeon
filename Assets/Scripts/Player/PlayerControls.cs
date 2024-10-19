8403d89bca59e537369108f8649969cc86bfed11
Vincent Chang <vincent.chang@hva.nl>
2024-10-19 16:28:40 +0200

    eAdded Double Jump Movement and Rotation update

--- Assets/Scripts/Player/PlayerControls.cs
+++ Assets/Scripts/Player/PlayerControls.cs
@@ -1,55 +1,100 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.IO.IsolatedStorage;
-using Unity.VisualScripting;
-using Unity.VisualScripting.InputSystem;
-using UnityEditorInternal;
 using UnityEngine;
 using UnityEngine.InputSystem;
+
 public class PlayerControls : MonoBehaviour
 {
     [Header("Components")]
     [SerializeField] private Rigidbody2D rb;
     [SerializeField] private PlayerInput playerInput;
 
-
     [SerializeField] private Transform groundCheck;
     [SerializeField] private LayerMask groundLayer;
 
     private Vector2 inputVector;
-    private bool isJumping;
 
+    [Header("Movement Settings")]
     public float movementSpeed = 5f;
-    public float jumpHeight = 10f;
-    
-    private void Update()
-    {
-        rb.velocity = inputVector;
 
-        if (isJumping)
-        {
-            if (IsGrounded()) isJumping = false;
-            inputVector.y -= 0.1f;
-        }
+    [Header("Jump Settings")]
+    public float jumpForce = 10f;
+    public int maxJumps = 2;
+
+    private int jumpCount;
+
+    private void FixedUpdate()
+    {
+        ResetJumpCount();
+
+        rb.velocity = new Vector2(inputVector.x * movementSpeed, rb.velocity.y);
     }
 
+    #region Move
     public void Move(InputAction.CallbackContext context)
     {
-        inputVector.x = context.ReadValue<Vector2>().x * movementSpeed;
-    }
+        inputVector.x = context.ReadValue<Vector2>().x;
 
+        HandleRotationOnMoving(inputVector.x);
+    }
+    #endregion
+
+    #region Rotation
+
+    private void HandleRotationOnMoving(float inputValue)
+    {
+        if (inputValue > 0)
+        {
+            transform.rotation = Quaternion.Euler(0, 0, 0);
+        }
+        else if (inputValue < 0)
+        {
+            transform.rotation = Quaternion.Euler(0, 180, 0);
+        }
+    }
+    #endregion
+    #region Jump
     public void Jump(InputAction.CallbackContext context)
+    {
+        if (context.performed && jumpCount < maxJumps)
+        {
+            ExecuteJump((float)context.duration);
+        }
+    }
+    private void ExecuteJump(float jump)
+    {
+        if (CheckJump())
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jump * jumpForce);
+            jumpCount++;
+        }
+    }   
+    private bool CheckJump()
+    {
+        return jumpCount < maxJumps - 1;
+    }
+    private void ResetJumpCount()
     {
         if (IsGrounded())
         {
-            isJumping = true;
-            inputVector.y = context.ReadValue<Vector2>().y * jumpHeight;
+            jumpCount = 0;
         }
     }
 
     private bool IsGrounded()
     {
-        print("fe");
         return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
     }
+
+    #endregion  
+    private void OnDrawGizmos()
+    {
+        if (groundCheck != null)
+        {
+
+            Gizmos.color = Color.red;
+            Gizmos.DrawWireSphere(groundCheck.position, 0.2f);
+        }
+    }
+
 }
