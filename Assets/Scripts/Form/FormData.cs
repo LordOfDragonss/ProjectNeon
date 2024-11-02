91a8f2588f3fbfac0d7ce73de2fae6c6ab7d0625
Vincent Chang <vincent.chang@hva.nl>
2024-11-02 16:19:49 +0100

    ADjusted FormData

--- Assets/Scripts/Form/FormData.cs
+++ Assets/Scripts/Form/FormData.cs
@@ -6,8 +6,8 @@ using UnityEngine;
 public class FormData : ScriptableObject
 {
     [SerializeField] private FormType formType;
-    [SerializeField] private Attack upAttack;
-    [SerializeField] private Attack downAttack;
-    [SerializeField] private Attack sideAttack;
-    [SerializeField] private Attack naturalAttack;
+    [SerializeField] private AttackData upAttack;
+    [SerializeField] private AttackData downAttack;
+    [SerializeField] private AttackData sideAttack;
+    [SerializeField] private AttackData naturalAttack;
 }
