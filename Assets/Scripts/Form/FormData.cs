824b8000c7d38616c478a90e79e180bb62f65197
Vincent Chang <vincent.chang@hva.nl>
2024-11-02 15:13:03 +0100

    ete

---
+++ Assets/Scripts/Form/FormData.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "file", menuName = "Form")]
+public class FormData : ScriptableObject
+{
+    [SerializeField] private FormType formType;
+
+
+
+}
